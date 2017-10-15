3decb66e85f5a73bda018f3892cccaff5886b2c7
Leo Cookson <10001481@bcs.net.nz>
2017-10-16 10:59:43 +1300

    1

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+//Must have for Lists
+using System.Collections.Generic;
+
+namespace C_
+{
+    class Program
+    {
+        //Make Lists
+        static List<string> cars = new List<string>{"Audi", "Mazda", "toyota", "Nissan"};
+        static List<string> drivers = new List<string>{"Matt", "Jeff", "Tom","Harry"};
+       
+       
+       //Prints string DisplayDriver to screen
+        static void Main(string[] args)
+        {
+           Console.WriteLine(DisplayDriver());
+           Console.WriteLine(DisplayDriver());
+           Console.WriteLine(DisplayDriver());
+        }
+        //Makes Random Number
+        static int GenerateRandomNumber(List<string> myList){
+             var rand = new Random();
+             //Lets random number be as long as list
+             return rand.Next(1, myList.Count);
+
+        }
+        //Create variables that = string name / Genertate Random numbers
+        static string DisplayDriver(){
+            var selectedCar = cars[GenerateRandomNumber(cars)];
+            var selectedDriver = drivers[GenerateRandomNumber(drivers)];
+            //Return Driver / Car
+            return $"{selectedDriver} is driving the = {selectedCar}";
+        }
+    }
+}
